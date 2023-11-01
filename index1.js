a4ca9cf421b34640321a3a278bd0ea4913b0938c
chatanyapra <chatanyapra@gmail.com>
2023-11-01 02:13:52 -0700

    created a cube in html

---
+++ index1.js
@@ -0,0 +1,8 @@
+var elem= document.getElementById('main');
+window.onmousemove = function(e){
+  // console.log(e.clientX, e.clientY);
+  var degX= ((e.clientX/window.innerWidth) * 360)-180;
+  console.log(degX);
+  var degY= ((e.clientY/window.innerHeight) * 360)-180;
+  elem.style.transform = "rotateX("+degY+"deg) rotateY("+degX+"deg)";
+}
