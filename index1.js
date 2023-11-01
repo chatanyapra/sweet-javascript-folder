565f993a49f591da396e057095157add1e1aeaad
chatanyapra <chatanyapra@gmail.com>
2023-11-01 02:29:48 -0700

    little change in cude design

--- index1.js
+++ index1.js
@@ -4,5 +4,5 @@ window.onmousemove = function(e){
   var degX= ((e.clientX/window.innerWidth) * 360)-180;
   console.log(degX);
   var degY= ((e.clientY/window.innerHeight) * 360)-180;
-  elem.style.transform = "rotateX("+degY+"deg) rotateY("+degX+"deg)";
+  elem.style.transform = "rotateX("+ -degY+"deg) rotateY("+degX+"deg)";
 }
